cf5bf909c1bed9fcff0a8f5bd28cf215d99c0681
Rashmi Mishra <rashmimishra97@gmail.com>
2026-02-07 00:20:56 +0530

    Update json content

--- app/app.py
+++ app/app.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Flask app running successfully!"})
 
+
 @app.route("/api")
 def api():
-    return jsonify({"status": "API working"})
+    return jsonify({
+        "project": "Flask To-Do Application",
+        "author": "Rashmi Mishra",
+        "version": "1.0",
+        "status": "API updated in rashmi_new branch"
+    })
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
